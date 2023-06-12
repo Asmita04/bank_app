3f6a09e91301dac37d53bd0bf609a29d9538c28d
Asmita Mhetre <asmitam0401@gmail.com>
2023-06-12 15:58:46 +0530

    Implemented Register Flow!

--- src/main/java/com/bluesky/bankapp/BankApplication.java
+++ src/main/java/com/bluesky/bankapp/BankApplication.java
@@ -11,6 +11,34 @@ import java.util.Scanner;
  *
  * */
 public class BankApplication {
+    int sequenceNumber;
+
+    public Map<String, User> userMap;
+    public HashMap<String, Integer> accountNumbers ;
+
+    public BankApplication() {
+        userMap=new HashMap<>();
+        accountNumbers = new HashMap<>();
+    }
+
+    public void addUserAccount(@NotNull User user) {
+        String adhaarNo = user.getAdhaarNo();
+        if (!accountNumbers.containsKey(adhaarNo)) {
+            accountNumbers.put(adhaarNo, 1);
+        } else {
+             sequenceNumber = accountNumbers.get(adhaarNo) + 1;
+            accountNumbers.put(adhaarNo, sequenceNumber);
+        }
+
+         sequenceNumber = accountNumbers.get(adhaarNo);
+        String accountNo = adhaarNo + "-" + sequenceNumber;
+        user.setAccountNo(accountNo);
+        userMap.put(accountNo, user);
+    }
+
+    public User getUserDetails(String accountNo) {
+        return userMap.get(accountNo);
+    }
 
     public Map<String, User> map;
     public HashMap<String, Integer> accountNumbers ;
@@ -44,7 +72,11 @@ public class BankApplication {
         int Choice;
 
         String userName;
+<<<<<<< Updated upstream
         String DOB;
+=======
+        String birthDate;
+>>>>>>> Stashed changes
         long mobileNo = 0;
         String adhaarNo;
         String accountNo;
@@ -52,11 +84,16 @@ public class BankApplication {
 
         System.out.println("Welcome to MHETRE's Bank Pvt Ltd!!");
         Scanner scan = new Scanner(System.in);
+<<<<<<< Updated upstream
       ///  Map<String, User> map = new HashMap<>();
    //     HashMap<String, Integer> accountNumbers = new HashMap<>();
         BankApplication account = new BankApplication();
 
         //   Iterator<user> it = obj.iterator();
+=======
+        BankApplication account = new BankApplication();
+
+>>>>>>> Stashed changes
 
         do {
             int temp = 1;
@@ -64,21 +101,33 @@ public class BankApplication {
             System.out.println("2.Remove Account");
             System.out.println("3.Transfer Money");
             System.out.println("4. View account details");
+<<<<<<< Updated upstream
             System.out.println("5.Display List of All Accounts");
             System.out.println("6.Exit");
+=======
+            System.out.println("5.Exit");
+>>>>>>> Stashed changes
             System.out.println("Enter Your Choice:");
             Choice = scan.nextInt();
 
             switch (Choice) {
                 case 1: {
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
                     System.out.println("Enter name:");
                     scan.nextLine();
                     userName = scan.nextLine();
 
+<<<<<<< Updated upstream
                     System.out.println("Enter DOB:");
                     DOB = scan.nextLine();
+=======
+                    System.out.println("Enter Birth Date:");
+                    birthDate = scan.nextLine();
+>>>>>>> Stashed changes
 
                     System.out.println("Enter Mobile:");
                     mobileNo = scan.nextLong();
@@ -91,6 +140,7 @@ public class BankApplication {
                     System.out.println("Enter Balance:");
                     balance = scan.nextInt();
 
+<<<<<<< Updated upstream
                     User user = new User(userName, DOB, mobileNo, adhaarNo, balance);
                     account.addUserAccount(user);
 
@@ -108,6 +158,11 @@ public class BankApplication {
 
                     map.put(accountNo, account);*/
 
+=======
+                    User user = new User(userName, birthDate, mobileNo, adhaarNo, balance);
+                    account.addUserAccount(user);
+
+>>>>>>> Stashed changes
                     break;
                 }
 
@@ -116,9 +171,15 @@ public class BankApplication {
                     System.out.println("Enter Account number to remove from list");
                     scan.nextLine();
                     accountNo = new String(scan.nextLine());
+<<<<<<< Updated upstream
                     if (account.map.containsKey(accountNo)) {
                         account.map.remove(accountNo);
                         System.out.println("1 Account Deleted!");
+=======
+                    if (account.userMap.containsKey(accountNo)) {
+                        account.userMap.remove(accountNo);
+                        System.out.println(" Account Deleted Successfully!!");
+>>>>>>> Stashed changes
                     } else {
                         System.out.println("Account is not present!");
                     }
@@ -135,8 +196,13 @@ public class BankApplication {
                     System.out.println("\nEnter Amount: ");
                     int dAmount = scan.nextInt();
 
+<<<<<<< Updated upstream
                     User sender =account. map.get(senderAccount);
                     User receiver = account.map.get(receiverAccount);
+=======
+                    User sender =account. userMap.get(senderAccount);
+                    User receiver = account.userMap.get(receiverAccount);
+>>>>>>> Stashed changes
                     if (sender.getBalance() >= dAmount) {
                         sender.setBalance(sender.getBalance() - dAmount);
                         receiver.setBalance(receiver.getBalance() + dAmount);
@@ -144,8 +210,13 @@ public class BankApplication {
                     } else {
                         System.out.println("Insufficient Balance!");
                     }
+<<<<<<< Updated upstream
                    account.map.put(senderAccount, sender);
                     account.map.put(receiverAccount, receiver);
+=======
+                    account.userMap.put(senderAccount, sender);
+                    account.userMap.put(receiverAccount, receiver);
+>>>>>>> Stashed changes
                     break;
                 }
                 case 4: {
@@ -156,14 +227,22 @@ public class BankApplication {
 
                     if(userdetails!=null){
                         System.out.println("Account No: \t\t" + "User Name: \t\t" + "DOB\t\t" + "Mobile No:\t\t" + "Adhaar No\t\t" + "Balance\t\n");
+<<<<<<< Updated upstream
                         System.out.println(accountNo + "  \t" + userdetails.userName + "  \t  " + userdetails.DOB + "  \t " + userdetails.mobileNo + "  \t" + userdetails.adhaarNo + "  \t" + userdetails.balance + "  \t\n");
+=======
+                        System.out.println(accountNo + " \t\t\t \t" + userdetails.userName + "  \t  " + userdetails.birthDate + "  \t " + userdetails.mobileNo + "  \t" + userdetails.adhaarNo + "  \t" + userdetails.balance + "  \t\n");
+>>>>>>> Stashed changes
                     }
                     else{
                         System.out.println("User not found!");
                     }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
                     break;
 
+<<<<<<< Updated upstream
                 }
                 case 5: {
 
@@ -186,5 +265,18 @@ public class BankApplication {
 
         }while (Choice != 6) ;
 
+=======
+                case 5:{
+
+                    System.out.println("exit");
+                    break;
+                }
+                default:
+                    throw new IllegalStateException(" I'm happy to help you on this journey please click one of the option below to get started!" );
+            }
+
+        }while (Choice != 5) ;
+
+>>>>>>> Stashed changes
     }
 }
--- src/main/java/com/bluesky/bankapp/User.java
+++ src/main/java/com/bluesky/bankapp/User.java
@@ -1,25 +1,53 @@
 package com.bluesky.bankapp;
 
 
+<<<<<<< Updated upstream
 public class User {
 
     public String userName;
     public String DOB;
+=======
+import java.util.*;
+import java.util.Scanner;
+class User {
+
+    public String userName;
+    public String birthDate;
+>>>>>>> Stashed changes
     public long mobileNo;
     public String adhaarNo;
     public String accountNo;
     public int balance;
 
+<<<<<<< Updated upstream
     public User(){}
     public User( String userName, String DOB, long mobileNo, String adhaarNo ,int balance) {
         this.userName = userName;
         this.DOB = DOB;
         this.mobileNo = mobileNo;
         this.adhaarNo = adhaarNo;
+=======
+    public User( String userName, String birthDate, long mobileNo, String adhaarNo ,int balance) {
+        this.userName = userName;
+        this.birthDate = birthDate;
+        this.mobileNo = mobileNo;
+        this.adhaarNo= adhaarNo;
+>>>>>>> Stashed changes
         this.balance = balance;
 
     }
 
+<<<<<<< Updated upstream
+=======
+    public String getAdhaarNo() {
+        return adhaarNo;
+    }
+
+    public void setAdhaarNo(String adhaarNo) {
+        this.adhaarNo = adhaarNo;
+    }
+
+>>>>>>> Stashed changes
     public int getBalance() {
         return balance;
     }
@@ -29,6 +57,7 @@ public class User {
     }
 
     public String getAccountNo() {
+<<<<<<< Updated upstream
         return accountNo ;
     }
 
@@ -42,12 +71,20 @@ public class User {
 
     public void setAdhaarNo(String adhaarNo) {
         this.adhaarNo = adhaarNo;
+=======
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+>>>>>>> Stashed changes
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "userName='" + userName + '\'' +
+<<<<<<< Updated upstream
                 ", DOB='" + DOB + '\'' +
                 ", mobileNo=" + mobileNo +
                 ", adhaarNo='" + adhaarNo + '\'' +
@@ -56,3 +93,13 @@ public class User {
     }
 }
 
+=======
+                ", Birth Date='" + birthDate + '\'' +
+                ", mobileNo=" + mobileNo +
+                ", adhaarNo=" + adhaarNo +
+                ", accountNo='" + accountNo + '\'' +
+                ", balance=" + balance +
+                '}'+'\n';
+    }
+}
+>>>>>>> Stashed changes
